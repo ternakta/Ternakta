5c8d43ed403b7188667bf5362d33db38d70be579
Maulidani <maulidanimahmud@gmail.com>
2021-10-06 17:55:08 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -110,7 +110,7 @@
                 <div class="row">
                   <div class="col-lg-12">
                     <h6>ternakta</h6>
-                    <h2>Cooming Soon...</h2>
+                    <h2>Cooming Soon</h2>
                     <p>
                       Dengan Ternakta semua bisa beternak
                       membantu para peternak pemula/kecil untuk
